d09aa950aeb8dee1f0a9ff06939f3a2c159d3e46
Benjamin Noje <beninoje@hotmail.com>
2024-11-01 12:05:42 -0400

    assests imported and splash screen

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+const Home = () => {
+    return (
+        <SafeAreaView>
+            <Text>This is the Home Screen</Text>
+        </SafeAreaView>
+    )
+}
+export default Home
