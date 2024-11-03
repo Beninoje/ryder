b70c5388f45451179b7ecb1a798aeec5b3e1887f
Beni <beninoje@hotmail.com>
2024-11-03 13:58:55 -0500

    added custom input field component

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import { Text } from 'react-native'
+import InputField from '@/components/input-field'
+import { icons, images } from '@/constants'
+import React, { useState } from 'react'
+import { Image, ScrollView, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    name:"",
+    email:"",
+    password:"",
+  })
   return (
-    <SafeAreaView>
-            <Text>SignUp</Text>
-        </SafeAreaView>
+    <ScrollView className='flex-1 bg-white'>
+            <View className='flex-1 bg-white'>
+              <View className='relative w-full h-[250px]'>
+                <Image source={images.signUpCar} className='z-0 w-full h-[250px]'/>
+                <Text className='absolute bottom-8 left-5 text-2xl text-black font-JakartaSemiBold'>Create Your Account</Text>
+              </View>
+              <View className='p-5 '>
+                <InputField
+                  label='Name'
+                  placeholder='Enter your name'
+                  icon={icons.person}
+                  value={form.name}
+                  onChangeText={(value)=>{
+                    setForm({
+                      ...form, 
+                      name: value,
+                    })
+                  }}
+                />
+              </View>
+            </View>
+    </ScrollView>
   )
 }
 
