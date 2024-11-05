a8b637f6ad9f0e935217de34a5311788534df2af
Benjamin Noje <beninoje@hotmail.com>
2024-11-05 11:34:15 -0500

    installed neon package and add API route

---
+++ app/(api)/user+api.ts
@@ -0,0 +1,39 @@
+import { neon } from '@neondatabase/serverless';
+
+
+
+// const posts = await sql('SELECT * FROM posts');
+
+export async function POST(request:Request)
+{
+    try { 
+        const sql = neon(`${process.env.DATABASE_URL}`);
+        const { name, email, clerkId } = await request.json();
+        if(!name || !email || !clerkId)
+        {
+            Response.json(
+                { error: "Missing required fields" },
+                { status:400 }
+            )
+        }
+        const response = await sql`
+            INSERT INTO users (
+                name,
+                email,
+                clerkId
+            )
+            VALUES (
+                ${name},
+                ${email},
+                ${clerkId}
+            )
+        `;
+        return new Response(JSON.stringify(response)),{
+            status:201
+        };
+    } catch (error) {
+         console.log(error);
+         return Response.json({error:error},{status:500});   
+    }
+        
+}
